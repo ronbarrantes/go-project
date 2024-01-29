506ac0f4a57abc83d18c34190a17f05df9f546b3
ronbarrantes <ronbarrantes@gmail.com>
2024-01-28 16:31:42 -0800

    Add a bunch of stuff

--- main.go
+++ main.go
@@ -1,34 +1,81 @@
 package main
 
-import "net/http"
+import (
+	"encoding/json"
+	"log"
+	"net/http"
 
-type APIServer struct {
-  listenAddr string
+	"github.com/gorilla/mux"
+)
+
+func WriteJSON(w http.ResponseWriter, status int, v any) error {
+	w.WriteHeader(status)
+	w.Header().Set("Content-Type", "application/json")
+	return json.NewEncoder(w).Encode(v)
 }
 
-func NewAPIServer(listenAddr string) *APIServer{
-  return &APIServer{
-    listenAddr: listenAddr,
-  }
+type apiFunc func(http.ResponseWriter, *http.Request) error
+
+type apiError struct {
+	Error string
+}
+
+func makeHTTPHandleFunc(f apiFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if err := f(w, r); err != nil {
+			WriteJSON(w, http.StatusBadRequest, apiError{Error: err.Error()})
+		}
+	}
+}
+
+type APIServer struct {
+	listenAddr string
+}
+
+func NewAPIServer(listenAddr string) *APIServer {
+	return &APIServer{
+		listenAddr: listenAddr,
+	}
+}
+
+func (s *APIServer) Run() {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/account", makeHTTPHandleFunc(s.handleAccount))
+
+	log.Println("Server on port:", s.listenAddr)
+
+	http.ListenAndServe(s.listenAddr, router)
 }
 
 func (s *APIServer) handleAccount(w http.ResponseWriter, r *http.Request) error {
-  return nil
+	if r.Method == "GET" {
+		return s.handleGetAccount(w, r)
+	}
+
+	if r.Method == "POST" {
+		return s.handleCreateAccount(w, r)
+	}
+
+	if r.Method == "DELETE" {
+		return s.handleDeleteAccount(w, r)
+	}
+
+	return nil
 }
 
-
 func (s *APIServer) handleGetAccount(w http.ResponseWriter, r *http.Request) error {
-  return nil
+	return nil
 }
 
 func (s *APIServer) handleCreateAccount(w http.ResponseWriter, r *http.Request) error {
-  return nil
+	return nil
 }
 
 func (s *APIServer) handleDeleteAccount(w http.ResponseWriter, r *http.Request) error {
-  return nil
+	return nil
 }
 
 func (s *APIServer) handleTransfer(w http.ResponseWriter, r *http.Request) error {
-  return nil
+	return nil
 }
