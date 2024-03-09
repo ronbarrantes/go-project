1291fd1d7dcdf9a46ee4b1367213125440ccd28f
ronbarrantes <ronbarrantes@gmail.com>
2024-03-09 13:53:28 -0800

    Add a hello route

--- api/index.go
+++ api/index.go
@@ -6,7 +6,13 @@ import (
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path == "/hello" {
+
+		fmt.Fprintf(w, "<p>hello from hello route</p>")
+		return
+	}
 	fmt.Fprintf(w, "<h1>Hello from vercel</h1>")
+
 }
 
 // func Handler(w http.ResponseWriter, r *http.Request) {
