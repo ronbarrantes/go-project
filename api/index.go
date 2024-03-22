bacf2c22f5d3188622d45cf50b796a3dc7e9decc
Ron Barrantes <ronbarrantes@gmail.com>
2024-03-22 00:15:49 +0000

    Add back the index.stuff

--- api/index.go
+++ api/index.go
@@ -9,12 +9,10 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 	count := 0
 
 	if r.URL.Path == "/hello" {
-
 		fmt.Fprintf(w, "<p>hello from hello route</p>")
 		return
 	}
 
 	count = count + 1
-
 	fmt.Fprintf(w, "<h1>Hello from vercel, clicked %d</h1>", count)
 }
