cf40fda0f95fb5734ca09770105b6af548271c68
ronbarrantes <ronbarrantes@gmail.com>
2024-10-23 17:21:32 -0600

    Create a UserStuff

--- main.go
+++ main.go
@@ -1,9 +1,66 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
 func main() {
-	server := NewAPIServer(":3003")
-	server.Run()
-	fmt.Println("HELLO")
+	PORT := "8000"
+
+	http.HandleFunc("/users", GetUsers)
+
+	fmt.Printf("Listening on %s", PORT)
+	log.Fatal(http.ListenAndServe(":"+PORT, nil))
 }
+
+// make the routes
+
+// USER STUFF
+
+type User struct {
+  ID string `json:"id"`
+  FirstName string `json:"firstName"`
+  LastName string `json:"lastName"`
+}
+
+func MakeUser (firstName , lastName string) *User{
+  user := User{
+    ID: ,
+   FirstName: firstName, 
+    LastName: lastName,
+  }
+  return &user 
+}
+
+// GetUsers
+func GetUsers(w http.ResponseWriter, r *http.Request) {
+  
+  users := []User{
+
+  } 
+
+
+}
+
+// GetUserById
+
+// MakeUser
+// UpdateUser
+// DeleteUser
+
+// STORE STUFF
+// CreateStores
+// GetStoreById
+// GetStores
+// UpdateStore
+// DeleteStore
+
+// AUTHENTICATION STUFF
+// IsAuthenticated
+// LogIn
+// LogOut
+// SignUp
+
+// DATABASE STUFF
