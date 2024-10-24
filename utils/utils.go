ec697586e1db25c614882bba1725373dc60a42c1
ronbarrantes <ronbarrantes@gmail.com>
2024-10-24 13:58:50 -0600

    Update utils

--- utils/utils.go
+++ utils/utils.go
@@ -1,17 +1,26 @@
 package utils
 
-import "math/rand"
+import (
+	"crypto/rand"
+	"log"
+	"math/big"
+	"strings"
+)
 
 func GenerateRandomId() string {
 	length := 10
 	keys := "1234567890abcdefghijklmnopqrstuvwxyz"
 
-	s := ""
+	var sb strings.Builder
 
 	for i := 0; i < length; i++ {
-		idx := rand.Intn(len(keys))
-		s += string(keys[idx])
+		// idx := rand.Intn(len(keys))
+		idx, err := rand.Int(rand.Reader, big.NewInt(int64(len(keys))))
+		if err != nil {
+			log.Fatalf("Failed to generate random index: %v", err)
+		}
+		sb.WriteByte(keys[idx.Int64()])
 	}
 
-	return s
+	return sb.String()
 }
